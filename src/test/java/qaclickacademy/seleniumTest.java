2349b680f3bbec34c38c9f12f13243b7a6efb924
abherijayaram <akhilakkumar93@gmail.com>
2020-07-16 22:24:51 +0530

    first commit

---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	
+			@Test
+			public void BrowserAUtomation()
+			{
+				System.out.println("BrowserAUtomation");	
+			}
+			@Test
+			public void elementsUi()
+			{
+				System.out.println("elementsUi");	
+			}
+
+}
